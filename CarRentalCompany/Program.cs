47abe151eeac29d383c7b070c108f7a3ba6690bd
Lars Bylund <lars.bylund@hotmail.com>
2018-09-17 14:51:46 +0200

    Add project files.

---
+++ CarRentalCompany/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarRentalCompany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
