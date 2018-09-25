168aacc99eb9d889878b166aefc4f0fb86b00e93
Lars Bylund <lars.bylund@hotmail.com>
2018-09-25 14:07:45 +0200

    Added Company method

--- CarRentalCompany/Program.cs
+++ CarRentalCompany/Program.cs
@@ -9,56 +9,60 @@ namespace CarRentalCompany
 
     class Program
     {
-
         static void Main(string[] args)
         {
-            Customer customer = new Customer();
-            int choice = 0;
-            bool validInput = false;
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("\n====== MENU ======\n");
-            Console.WriteLine("1. Choose Vehicle");
-            Console.WriteLine("2. Rental Period");
-            Console.WriteLine("3. Check Price range");
-            Console.WriteLine("\n4. Exit\n");
 
-            Console.WriteLine("Your input: ");
+            Company LambdaCarRental = new Company();
+            LambdaCarRental.PrintOurCars();
+            Console.ReadLine();
 
-            do
-            {
-                Console.ResetColor();
-                string inputValue = Console.ReadLine();
-                if (int.TryParse(inputValue, out choice) && choice < 5 && choice > 0)
-                {
-                    validInput = true;
-                    break;
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    validInput = false;
-                    Console.WriteLine("There are only 4 choices.");
-                }
-            } while (true);
-            switch (choice)
-            {
-                case 1:
-                    Console.WriteLine("");
-                    break;
+            //    Customer customer = new Customer();
+            //    int choice = 0;
+            //    bool validInput = false;
+            //    Console.ForegroundColor = ConsoleColor.Green;
+            //    Console.WriteLine("\n====== MENU ======\n");
+            //    Console.WriteLine("1. Choose Vehicle");
+            //    Console.WriteLine("2. Rental Period");
+            //    Console.WriteLine("3. Check Price range");
+            //    Console.WriteLine("\n4. Exit\n");
 
-                case 2:
-                    Console.WriteLine("");
-                    break;
+            //    Console.WriteLine("Your input: ");
 
-                case 3:
-                    Console.WriteLine("");
-                    break;
+            //    do
+            //    {
+            //        Console.ResetColor();
+            //        string inputValue = Console.ReadLine();
+            //        if (int.TryParse(inputValue, out choice) && choice < 5 && choice > 0)
+            //        {
+            //            validInput = true;
+            //            break;
+            //        }
+            //        else
+            //        {
+            //            Console.ForegroundColor = ConsoleColor.Red;
+            //            validInput = false;
+            //            Console.WriteLine("There are only 4 choices.");
+            //        }
+            //    } while (true);
+            //    switch (choice)
+            //    {
+            //        case 1:
+            //            Console.WriteLine("");
+            //            break;
 
-                case 4:
-                    Environment.Exit(0);
-                    break;
-            }
-            customer.CheckifEligible();
+            //        case 2:
+            //            Console.WriteLine("");
+            //            break;
+
+            //        case 3:
+            //            Console.WriteLine("");
+            //            break;
+
+            //        case 4:
+            //            Environment.Exit(0);
+            //            break;
+            //    }
+            //    customer.CheckifEligible();
 
             Console.ReadLine();
 
