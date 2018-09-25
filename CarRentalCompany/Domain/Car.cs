61e3d3ed542ffd087534ea55540dcbc45b1c2e7b
Lars Bylund <lars.bylund@hotmail.com>
2018-09-25 10:26:56 +0200

    Company class added and minor changes in methods

--- CarRentalCompany/Domain/Car.cs
+++ CarRentalCompany/Domain/Car.cs
@@ -9,16 +9,17 @@ namespace CarRentalCompany.Domain
         string TypeOfCar;  // Sedan, MPV, Convertible
         public string DriversEligibilityNeeded { get; } = "B";
 
-        public Car(string model, string registrationNumber, string vehicleCategory, bool isRented, string typeOfCar)
-            : base(model, registrationNumber, vehicleCategory, isRented)
+        public Car(string brand, string model, string registrationNumber, string vehicleCategory, bool isRented, string typeOfCar)
+            : base(brand, model, registrationNumber, vehicleCategory, isRented)
         {
             TypeOfCar = typeOfCar;
         }
 
         public override void PrintVehicleData()
         {
-            Console.WriteLine($"The car is a: {TypeOfCar}");
+            Console.WriteLine($"The car is of brand: {Brand}");
             Console.WriteLine($"The car is of model: {Model}");
+            Console.WriteLine($"The car is a: {TypeOfCar}");
             Console.WriteLine($"The car has registration number: {RegistrationNumber}");
             Console.WriteLine($"The car is {((IsRented) ? "not avalible" : "avalible")}");
         }
